5f59a2e3377f3803022fbf01a1b0279cbd828a98
Evandro <evandrocalado07@gmail.com>
2023-01-16 00:27:53 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,92 @@
+import { Link } from "react-router-dom";
+import { Col, Container, ListGroup, ListGroupItem, Row } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+import "../../styles/Footer.css";
+
 const Footer = () => {
-  return ( 
-    <div>
-      <h1>Footer</h1>
-    </div>
-   );
-}
- 
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="logo footer__logo text-start">
+              <img src={logo} alt="Logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus accusamus
+              </p>
+            </div>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="delivery__time-list">
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00 am - 11:00 pm</p>
+              </ListGroupItem>
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Friday - Saturday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="delivery__time-list">
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Location: ZindaBazar, Sylhet-3100, Bangladesh</span>
+              </ListGroupItem>
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Phone: 01712345678</span>
+              </ListGroupItem>
+              <ListGroupItem className="delivery__time-item border-0 ps-0">
+                <span>Email: example@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2022, website made by Evandrp Calado. All Rights
+              Resenved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center justify-content-end gap-4">
+              <p className="m-0">Follow: </p>
+              <span>
+                <Link to="https://www.facebook.com/profile.php?id=100013304342694">
+                  <i class="ri-facebook-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="https://github.com/EvandroCalado">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="https://www.linkedin.com/in/evandro-calado/">
+                  <i class="ri-linkedin-line"></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
 export default Footer;
